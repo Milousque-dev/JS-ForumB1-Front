78b0cea50c154d9c4f6446c6374900c2ffdfdc6f
Wilfrid <wilfrid.delamare@ynov.com>
2026-06-05 16:12:42 +0200

    errorRegister

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module forum
 
 go 1.25.0
-
-require github.com/mattn/go-sqlite3 v1.14.44
--- handlers/register.go
+++ handlers/register.go
@@ -1,12 +1,22 @@
 package handlers
 
 import (
-	"net/http"
 	"fmt"
+	"forum/models"
+	"net/http"
 )
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "register.tmpl", nil)
+	err := ""
+
+	if r.URL.Query().Get("error") == "1" {
+		err = "wrong email or username or password"
+	}
+
+	data := models.TemplateData{
+		Error: err,
+	}
+	RenderTemplate(w, "register.tmpl", data)
 }
 
 func PostRegisterHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,15 +24,19 @@ func PostRegisterHandler(w http.ResponseWriter, r *http.Request) {
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 
-	http.SetCookie(w, &http.Cookie{
-		Name: "session_id",
-		Value: "session_nbr",
-		Path: "/",
-		MaxAge: 3600,
-		HttpOnly: true,
-	})
-		
+	if email == "wilfrid.delamare@ynov.com" && len(password) <= 25 {
+		http.SetCookie(w, &http.Cookie{
+			Name: "session_id",
+			Value: "session_nbr",
+			Path: "/",
+			MaxAge: 3600,
+			HttpOnly: true,
+		})
+
 	fmt.Println(email, username, password) //faudra envoyer dans la bdd ces datas...
 
 	http.Redirect(w, r, "/", http.StatusSeeOther) // redirige vers index avec 303
+	}	
+
+	http.Redirect(w, r, "/register?error=1", http.StatusSeeOther)
 }
